56e0e98e6dec96752b926cd15494aaa2920a838f
Ebony <ebony.furr@yahoo.com>
2020-11-02 20:16:27 -0600

    commit tree 809714f988c234aed941437d5debdb9ffffaa0a0

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+    def initialize(title) #setter method and initalization
+        @title = title
+    end
 
+    def title   #getter method 
+        @title
+    end
+
+    def author=(author) #setter method
+        @author = author
+    end
+
+    def author  #getter method 
+        @author
+    end
+
+    def page_count=(num) #setter method
+        @page_count = num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
